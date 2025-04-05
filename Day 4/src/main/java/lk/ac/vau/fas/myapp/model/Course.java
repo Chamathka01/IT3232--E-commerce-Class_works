25d348fd466a97d4c7097ada81dfe633af8e2073
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-04-05 12:56:49 +0530

    Generate constractors

--- Day 4/src/main/java/lk/ac/vau/fas/myapp/model/Course.java
+++ Day 4/src/main/java/lk/ac/vau/fas/myapp/model/Course.java
@@ -1,4 +1,14 @@
 package lk.ac.vau.fas.myapp.model;
 public class Course {
+    private String courseId;
+	private String courseName;
+	private String lecName;
+    
+    public Course(String courseId, String courseName, String lecName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.lecName = lecName;
+    }
+
     
 }
