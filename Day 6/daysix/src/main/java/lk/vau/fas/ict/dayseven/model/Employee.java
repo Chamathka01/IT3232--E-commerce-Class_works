415ba0b730525f688d8b41738a3cc0b70e0f9339
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-03 15:44:00 +0530

    Added field for Employee

--- Day 6/daysix/src/main/java/lk/vau/fas/ict/dayseven/model/Employee.java
+++ Day 6/daysix/src/main/java/lk/vau/fas/ict/dayseven/model/Employee.java
@@ -1,3 +1,8 @@
+package lk.vau.fas.ict.daysix.model;
 public class Employee {
-    
+    private String empNo;
+	private String name;
+	private int age;
+	private String gender;
+	private double salary;
 }
