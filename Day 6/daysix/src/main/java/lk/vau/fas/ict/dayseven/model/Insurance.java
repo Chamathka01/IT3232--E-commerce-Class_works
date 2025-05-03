798d482dd588cd6b662cba2f75e06de9fd06dd27
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-03 15:59:22 +0530

    Insurance relationships

--- Day 6/daysix/src/main/java/lk/vau/fas/ict/dayseven/model/Insurance.java
+++ Day 6/daysix/src/main/java/lk/vau/fas/ict/dayseven/model/Insurance.java
@@ -1,5 +1,17 @@
 package lk.vau.fas.ict.daysix.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+@Entity
 public class Insurance {
+    @Id
+	@Column(name="Insurance_id")
     private int id;
+    @Column(name="Insurance_type")
     private String type;
+
+    @OneToOne
+	private Employee employees;
 }
