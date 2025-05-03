691404fdd0be9094c225aebad822e9363445f292
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-03 16:00:41 +0530

    Added realshipships for Project

--- Day 6/daysix/src/main/java/lk/vau/fas/ict/dayseven/model/Project.java
+++ Day 6/daysix/src/main/java/lk/vau/fas/ict/dayseven/model/Project.java
@@ -1,7 +1,22 @@
 package lk.vau.fas.ict.daysix.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+
+@Entity
 public class Project {
+    @Id
     private int id;
 	private String name;
 	private long totalcost;
+
+    @ManyToMany
+	private List<Employee>employees;
     
 }
