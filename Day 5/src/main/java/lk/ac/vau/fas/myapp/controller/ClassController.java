f2b57c341d8e7a2c26a309cb9c68c08746f67aeb
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-03 13:23:42 +0530

    Inserting key-value pairs into a map using the put() method

--- Day 5/src/main/java/lk/ac/vau/fas/myapp/controller/ClassController.java
+++ Day 5/src/main/java/lk/ac/vau/fas/myapp/controller/ClassController.java
@@ -1,3 +1,14 @@
+import lk.ac.vau.fas.myapp.model.Course;
+
+@RestController
+@RequestMapping("/course")
 public class ClassController {
-    
+    Course ecommerce = new Course("IT3232","E-Commerce",2);
+    Course webappservice = new Course("IT2234","Web Service And Server Technology",4);
+    Course webapplication = new Course("CSC3132","Web Application Development",2);
+
+    getMap().put(ecommerce.getCode(), ecommerce);
+    getMap().put(webappservice.getCode(),webappservice);
+    getMap().put(webapplication.getCode(),webapplication);
+
 }
