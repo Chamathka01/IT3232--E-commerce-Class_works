74968f9925b4d9559d007e4c683cd430f6a09f80
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-03 15:57:15 +0530

    Create relationships of Employee

--- Day 6/daysix/src/main/java/lk/vau/fas/ict/dayseven/model/Employee.java
+++ Day 6/daysix/src/main/java/lk/vau/fas/ict/dayseven/model/Employee.java
@@ -1,8 +1,24 @@
 package lk.vau.fas.ict.daysix.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Employee {
+    @Id
     private String empNo;
 	private String name;
 	private int age;
 	private String gender;
 	private double salary;
+
+    @ManyToOne
+	private Department department;
+
+    @ManyToMany(mappedBy="employees")
+	private List<Project>projects;
 }
