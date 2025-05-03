cac05dc63702ff10c32bbebb5641a602a44793e9
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-03 13:33:34 +0530

    Import Statements

--- Day 5/src/main/java/lk/ac/vau/fas/myapp/controller/CRUDController.java
+++ Day 5/src/main/java/lk/ac/vau/fas/myapp/controller/CRUDController.java
@@ -1,5 +1,13 @@
+package lk.ac.vau.fas.myapp.controller;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class CRUDController<K,T> {
     private Map<K, T> object = new HashMap<K,T>();
--- Day 5/src/main/java/lk/ac/vau/fas/myapp/controller/ClassController.java
+++ Day 5/src/main/java/lk/ac/vau/fas/myapp/controller/ClassController.java
@@ -1,4 +1,4 @@
-package lk.ac.vau.fas.ict.controller;
+package lk.ac.vau.fas.myapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
