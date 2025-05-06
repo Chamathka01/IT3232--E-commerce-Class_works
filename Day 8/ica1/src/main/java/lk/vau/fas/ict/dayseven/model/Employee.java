b67ce9cd163754ecf83d096ccace0bd02df950f9
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-06 09:34:29 +0530

    Created Employee.java

--- Day 8/ica1/src/main/java/lk/vau/fas/ict/dayseven/model/Employee.java
+++ Day 8/ica1/src/main/java/lk/vau/fas/ict/dayseven/model/Employee.java
@@ -1,3 +1,23 @@
-public class Employee {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Employee extends Person{
+    @Id
+    private Long studentId;
+    private String jobPosition;
+    
+    public Long getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+    public String getJobPosition() {
+        return jobPosition;
+    }
+    public void setJobPosition(String jobPosition) {
+        this.jobPosition = jobPosition;
+    }
+
     
 }
