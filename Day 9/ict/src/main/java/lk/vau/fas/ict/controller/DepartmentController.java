82bc784596bf62b27de16264316591466f4b1f4e
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-18 09:31:07 +0530

    Get,post methods for Department

--- Day 9/ict/src/main/java/lk/vau/fas/ict/controller/DepartmentController.java
+++ Day 9/ict/src/main/java/lk/vau/fas/ict/controller/DepartmentController.java
@@ -1,7 +1,12 @@
 package lk.vau.fas.ict.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +15,31 @@ import lk.vau.fas.ict.model.Department;
 @RequestMapping("/dept")
 public class DepartmentController {
     @Autowired
-    @GetMapping
-	public List<Department>getDepts(){
-		return repo.findAll();
+    @PostMapping
+	public String addDept(@RequestBody Department department) {
+		repo.save(department);
+		return "New department added";
 	}
+
+    @PutMapping("/{id}")
+	public String updateDept(@PathVariable("id") int id,@RequestBody Department department) {
+		if(repo.findById(id).isEmpty()) {
+			return "Couldn't find the department";
+		}
+		repo.save(department);
+		return "The department updated";
+	}
+
+    @DeleteMapping("/{id}")
+	public String deleteDept(@PathVariable("id") int id) {
+			if(repo.findById(id).isEmpty()) {
+				return "Couldn't find the department";
+			}
+			repo.deleteById(id);
+			return "The department deleted";
+	
+	}
+    
 
 
 }
