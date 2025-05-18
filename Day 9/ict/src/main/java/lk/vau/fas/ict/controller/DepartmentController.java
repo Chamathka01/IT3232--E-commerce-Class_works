a48f9c3fd006da74ea66e54b01a107754a8d40d1
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-18 09:37:29 +0530

    Retrieve Department by ID

--- Day 9/ict/src/main/java/lk/vau/fas/ict/controller/DepartmentController.java
+++ Day 9/ict/src/main/java/lk/vau/fas/ict/controller/DepartmentController.java
@@ -1,20 +1,42 @@
 package lk.vau.fas.ict.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//import lk.ac.vau.fas.ict.DepartmentRepo;
 import lk.vau.fas.ict.model.Department;
+import lk.vau.fas.ict.service.DepartmentService;
 @RestController
 @RequestMapping("/dept")
 public class DepartmentController {
     @Autowired
+    public DepartmentService service;
+
+    public ResponseEntity<List<Department>>getDepts(){
+		return new ResponseEntity<List<Department>>
+			(service.getDepts(),HttpStatus.OK);
+	}
+    @GetMapping("/{id}")
+	public ResponseEntity<Department>getDepts(@PathVariable("id") int id){
+		if(service.getDept(id)== null) {
+			return new ResponseEntity<Department>
+			(service.getDept(id),HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Department>
+		(service.getDept(id),HttpStatus.OK);
+	}
+    /* 
     @PostMapping
 	public String addDept(@RequestBody Department department) {
 		repo.save(department);
@@ -39,7 +61,7 @@ public class DepartmentController {
 			return "The department deleted";
 	
 	}
-    
+    */
 
 
 }
