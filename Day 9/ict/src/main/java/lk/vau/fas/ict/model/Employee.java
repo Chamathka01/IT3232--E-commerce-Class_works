6be931b7d63e8faee3cc4643e3e8f0f3eb7dfaba
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-18 09:07:51 +0530

    create getters and setters

--- Day 9/ict/src/main/java/lk/vau/fas/ict/model/Employee.java
+++ Day 9/ict/src/main/java/lk/vau/fas/ict/model/Employee.java
@@ -1,5 +1,81 @@
 package lk.vau.fas.ict.model;
 
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long EmpId;
+    private String EmpName;
+    private String Job;
+    private double Salary;
+    private Date DOB;
+    private Department department;
+
+    public Employee() {}
+
+    public Employee(Long empId, String empName, String job, double salary, Date dOB, Department department) {
+        EmpId = empId;
+        EmpName = empName;
+        Job = job;
+        Salary = salary;
+        DOB = dOB;
+        this.department = department;
+    }
+
+    public Long getEmpId() {
+        return EmpId;
+    }
+
+    public void setEmpId(Long empId) {
+        EmpId = empId;
+    }
+
+    public String getEmpName() {
+        return EmpName;
+    }
+
+    public void setEmpName(String empName) {
+        EmpName = empName;
+    }
+
+    public String getJob() {
+        return Job;
+    }
+
+    public void setJob(String job) {
+        Job = job;
+    }
+
+    public double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(double salary) {
+        Salary = salary;
+    }
+
+    public Date getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(Date dOB) {
+        DOB = dOB;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
     
+
 }
