5421bbcc03cdb6b87befa0953370250def3a6961
Chamathka01 <chamathkahettiarachchi@gmail.com>
2025-05-18 09:12:10 +0530

    Add relationships for Department

--- Day 9/ict/src/main/java/lk/vau/fas/ict/model/Department.java
+++ Day 9/ict/src/main/java/lk/vau/fas/ict/model/Department.java
@@ -1,5 +1,40 @@
 package lk.vau.fas.ict.model;
 
+import java.sql.Date;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 public class Department {
+    @Id
+    @Column(name="dept_id")
+    private int id;
+    @Column(nullable=false)
+    private String name;
+    private Date established;
+    @OneToMany(mappedBy="department")
+	private List<Employee>employees;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Date getEstablished() {
+        return established;
+    }
+    public void setEstablished(Date established) {
+        this.established = established;
+    }
+
     
 }
